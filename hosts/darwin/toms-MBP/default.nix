cff9438a2f310be861af56435a40c9c0385d68ae
5wiley <thomaswileycotton@gmail.com>
2026-01-23 08:31:26 -0700

    temp disable big parrellel to let the build go

--- hosts/darwin/toms-MBP/default.nix
+++ hosts/darwin/toms-MBP/default.nix
@@ -34,6 +34,12 @@ in {
     })
   ];
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    firefox-unwrapped = pkgs.firefox-unwrapped.override {
+      enableParallelBuilding = false;
+    };
+  };
+
   # stdenv.mkDerivation.NIX_LDFLAGS = "-headerpad_max_install_names";
 
   homebrew = {
