817cda61a388040bf4c7904dc745198161595b7f
tom-wiley-cotton <tom@tomwileycotton.com>
2026-02-25 17:19:19 -0700

    tempfile neovim edition leader ft or scratch sesh

--- home/tomcotton/config/.config/nvim/lua/keymaps.lua
+++ home/tomcotton/config/.config/nvim/lua/keymaps.lua
@@ -9,3 +9,17 @@ vim.keymap.set("n", "<space>O", "printf('m`%sO<ESC>``', v:count1)", {
   expr = true,
   desc = "insert line above",
 })
+
+-- Map <leader>ft to open a temporary markdown file
+vim.keymap.set('n', '<leader>ft', function()
+    local temp_file = vim.fn.expand("~/tmp/neotemp.md")
+    local temp_dir = vim.fn.fnamemodify(temp_file, ":h")
+
+    -- Create the directory if it doesn't exist (p flag handles parents)
+    if vim.fn.isdirectory(temp_dir) == 0 then
+        vim.fn.mkdir(temp_dir, "p")
+    end
+
+    -- Open the file in the current buffer
+    vim.cmd("edit " .. temp_file)
+end, { desc = "Open temporary markdown file" })
