005f8348cc3e43072cb341a86761cee066a4078d
Bob Cotton <bob.cotton@gmail.com>
2026-03-21 05:45:19 -0600

    fix go.sum hash for beads v0.61.0

--- home/bcotton.nix
+++ home/bcotton.nix
@@ -653,6 +653,7 @@
           version = "0.61.0";
           src = inputs.beads;
           subPackages = ["cmd/bd"];
+          vendorHash = "sha256-R8x4Jlelz50I533PlzzGn04hrGtUs2aGXBTXkEkXI08=";
           nativeBuildInputs = [pkgs.git pkgs.pkg-config];
           buildInputs = [pkgs.icu];
           doCheck = false;
