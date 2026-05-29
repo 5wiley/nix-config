ea5381b490bf1bd7cda863f3a0becc7e30b07f2e
Larry <larry@nix-02>
2026-05-29 07:11:15 -0600

    feat(home): add Nix workflow tools for bcotton

--- flake.nix
+++ flake.nix
@@ -30,6 +30,9 @@
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
     nixos-shell.url = "github:Mic92/nixos-shell";
 
     ghostty.url = "github:ghostty-org/ghostty";
